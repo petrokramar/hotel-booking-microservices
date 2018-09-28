476820d19aabbe604e685b38a8f3acb5fe7be4a8
starikivan <peter.v.kramar@gmail.com>
2018-09-28 14:10:44 +0300

    add discovery server

---
+++ discovery/src/main/java/com/hotelbooknig/demo/DiscoveryServerApp.java
@@ -0,0 +1,12 @@
+package com.hotelbooknig.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApp.class, args);
+	}
+}
