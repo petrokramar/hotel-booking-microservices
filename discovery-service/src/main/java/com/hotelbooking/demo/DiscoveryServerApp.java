51e094d76f7774ec44194fc41d8d4a27ac3fb884
starikivan <peter.v.kramar@gmail.com>
2018-09-28 15:59:12 +0300

    build.gradle fix

--- discovery-service/src/main/java/com/hotelbooking/demo/DiscoveryServerApp.java
+++ discovery-service/src/main/java/com/hotelbooking/demo/DiscoveryServerApp.java
@@ -1,11 +1,12 @@
-package com.hotelbooknig.demo;
+package com.hotelbooking.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApp {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApp.class, args);
 	}
