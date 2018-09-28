4bed2a38c9f7436ee706dcad794804968bb6e68e
starikivan <peter.v.kramar@gmail.com>
2018-09-28 22:52:14 +0300

    add gateway service

---
+++ gateway-service/src/main/java/com/hotelbooking/demo/GatewayApp.java
@@ -0,0 +1,17 @@
+package com.hotelbooking.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApp.class, args);
+	}
+}
