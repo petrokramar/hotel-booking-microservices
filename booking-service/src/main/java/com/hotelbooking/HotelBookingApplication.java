00faf100a8f832251afcf34cbbd124e704e66ee5
starikivan <peter.v.kramar@gmail.com>
2018-10-01 15:06:18 +0300

    add eureka client to booking-service

--- booking-service/src/main/java/com/hotelbooking/HotelBookingApplication.java
+++ booking-service/src/main/java/com/hotelbooking/HotelBookingApplication.java
@@ -2,8 +2,10 @@ package com.hotelbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HotelBookingApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelBookingApplication.class, args);
